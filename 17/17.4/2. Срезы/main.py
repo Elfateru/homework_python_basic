5d34d036d4e87ddf608e4556c840399b941c3cba
Sergey Litvinchev <176Sm0ke@gmail.com>
2022-08-01 22:05:56 +0300

    17.4.2 done

--- 17/17.4/2. Срезы/main.py
+++ 17/17.4/2. Срезы/main.py
@@ -1,11 +1,8 @@
-members = int(input('Кол-во участников: '))
-teams = int(input('Кол-во человек в команде: '))
-member = []
-num = 1
-if members % teams == 0:
-    for _ in range(members // teams):
-        member.append(list(range(num, num + teams)))
-        num += teams
-    print(member)
-else:
-    print(members, 'участников невозможно поделить на команды по', teams, 'человек!')
+nums = [48, -10, 9, 38, 17, 50, -5, 43, 46, 12]
+
+print(nums[:5])
+print(nums[:-2])
+print(nums[0::2])
+print(nums[1::2])
+print(nums[::-1])
+print(nums[-1:0:-2])
